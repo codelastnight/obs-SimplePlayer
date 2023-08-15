9a5466f371bd9b0e1700a88d3d978eef759432b2
last night <fujizhang2525@gmail.com>
2023-08-15 06:02:48 -0400

    delete resources and add files

--- src-server/src/App.svelte
+++ src-server/src/App.svelte
@@ -1,10 +1,52 @@
 <script lang="ts">
 import Lily from './lib/Lily.svelte';
+import { io } from 'socket.io-client';
+
+const socket = io();
+const delay = 2000; //120000
+
+let timerId = setTimeout(animstart, delay);
+let title = 'test';
+let tracklisting = 'test2';
+function animstart() {
+    // const animate = randomize(animList)[0]
+    // main.classList.add(animate)
+    // timerId = setTimeout(() => animend(animate), 2000);
+}
+
+socket.on('onload', function (msg) {
+    clearTimeout(timerId);
+    timerId = setTimeout(animstart, delay);
+
+    socket.emit('whoiam', 'reciever');
+});
+socket.on('whouare', function (msg) {
+    socket.emit('ask4update', 'pls');
+});
+socket.on('update', function (msg) {
+    console.log(msg);
+
+    if (!msg.animRand) clearTimeout(timerId);
+    else {
+        clearTimeout(timerId);
+        timerId = setTimeout(animstart, delay);
+    }
+
+    title = msg.title;
+    tracklisting = msg.track;
+});
+socket.on('disconnect', function () {
+    title = 'disconnected';
+    tracklisting = 'try refreshing browser source or app?';
+    clearTimeout(timerId);
+});
 </script>
 
 <main>
     <div>
         <Lily />
+        <p>{title}</p>
+        <p>{tracklisting}</p>
     </div>
 </main>
 
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,61 +1,49 @@
 import express from 'express';
 import { createServer } from 'http';
 
-import { join } from 'path'
+import { join } from 'path';
 import { Server } from 'socket.io';
 
-
 process.parentPort.on('message', (e) => {
-    const [port] = e.ports
-    console.log(e.data.message)
-    process.parentPort.postMessage('what is this')
-})
-
+    const [port] = e.ports;
+    console.log(e.data.message);
+    process.parentPort.postMessage('what is this');
+});
 
 let app = express();
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 httpServer.listen(9990);
-console.log(join(__dirname + '../../resources'))
+console.log(join(__dirname + '../../resources'));
 app.use('/', express.static(join(__dirname, '../../resources')));
 
 app.get('/', function (req, res) {
-
-    res.sendFile(join(__dirname, '../../resources/serve.html'));
-
+    res.sendFile(join(__dirname, '../../resources/index.html'));
 });
 io.on('connection', (socket) => {
-    onNewSocketConnection(socket)
+    onNewSocketConnection(socket);
 });
 function onNewSocketConnection(socket) {
-    io.emit('onload', "ur connected"); // This will emit the event to all connected sockets
+    io.emit('onload', 'ur connected'); // This will emit the event to all connected sockets
     socket.on('disconnect', () => {
         console.log('user dc');
-
-
     });
     socket.on('whoiam', (msg) => {
         if (msg === 'sender') {
             io.to(socket.id).emit('whouare', `things look good and running`);
-            socket.join("senders");
-
+            socket.join('senders');
         } else {
             io.to(socket.id).emit('whouare', `things look good and running`);
-            socket.join("recievers");
+            socket.join('recievers');
         }
-
     });
     socket.on('ask4update', (msg) => {
         if (msg === 'pls') {
-            socket.to('senders').emit('ask4update', msg)
+            socket.to('senders').emit('ask4update', msg);
         }
-    })
+    });
     socket.on('update', (msg) => {
-        socket.to('recievers').emit('update', msg)
-
-    })
-
-
-
+        socket.to('recievers').emit('update', msg);
+    });
 }
