9781d2a668a89d23aeda2f254ad54a621835fe1b
last night <fujizhang2525@gmail.com>
2023-08-15 05:49:04 -0400

    idle n shit

---
+++ src-server/src/lib/Froggie.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+const images = ['/frog_smug.png', '/frog_talk.png'];
+
+const states = {
+    idle: {
+        src: images[0]
+    },
+    prespeak: {
+        src: images[1]
+    },
+    speak: {
+        src: images[0]
+    }
+};
+type State = keyof typeof states;
+
+export let state: State = 'idle';
+
+interface stateData {
+    src: string;
+}
+</script>
+
+<img class="lily" src={states[state].src} alt="magical froggie lily chibi" />
+
+<style>
+.lily {
+    width: 10rem;
+}
+</style>
---
+++ src-server/src/lib/Lily.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+const images = ['/lily_listen.png', '/lily_talk.png'];
+
+interface stateData {
+    src: string;
+    class: string;
+}
+const states: { [key: string]: stateData } = {
+    idle: {
+        src: images[1],
+        class: ''
+    },
+    talk: {
+        src: images[1],
+        class: ''
+    },
+    listen: {
+        src: images[0],
+        class: 'listen'
+    }
+};
+type State = keyof typeof states;
+export let state: State = 'idle';
+</script>
+
+<img
+    class="lily {states[state].class}"
+    src={states[state].src}
+    alt="magical froggie lily chibi"
+/>
+
+<style>
+.lily {
+    width: 10rem;
+    animation-iteration-count: infinite;
+}
+
+.listen {
+    animation-name: listen;
+    animation-duration: 2s;
+    animation-timing-function: steps(2, jump-none);
+}
+@keyframes listen {
+    from {
+        transform: scaleX(-1);
+    }
+    to {
+        transform: scaleX(1);
+    }
+}
+</style>
